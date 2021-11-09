38a7c8586d67cb26057fb70ac377cf7cdea1fddc
abdurashiidov <abdurashiidov18@gmail.com>
2021-11-09 14:24:14 -0700

    add var

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 var select = document.querySelectorAll(".currency");
+var btn = document.getElementById("btn");
+var input = document.getElementById("input");
+var result = document.getElementById("result");
+
