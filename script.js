51e37b3ee10c5b15c9b70df5238a7c1ed9a39b89
abdurashiidov <abdurashiidov18@gmail.com>
2021-11-09 14:27:54 -0700

    add info

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ var btn = document.getElementById("btn");
 var input = document.getElementById("input");
 var result = document.getElementById("result");
 
+var dollars = 1700
+var rubls = 2300
+var euros = 1200
+button.addEventListener("click", () => {
+let select = document.querySelector(".form-select")
+    let value = select.value
+    if (value == "euro"){
+        console.log('ok'); 
+        sum * euros
+        heading.textContent = sum.value*euros
+    }
+    else if  (value == "rubl"){
+        sum * rubls
+        heading.textContent = sum.value*rubls
+    }
+    else if  (value == "dollar"){
+        sum * dollars
+        heading.textContent = sum.value*dollars
+    }
+})
